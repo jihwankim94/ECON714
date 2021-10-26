7af210f23e38d23812284c3ddd8826c7b4a1abdb
jihwankim94 <93222299+jihwankim94@users.noreply.github.com>
2021-10-26 15:49:36 -0400

    Delete Homework1 directory

--- Homework1/3/rosenbrock.m
+++
@@ -1,9 +0,0 @@
-function [f df hf] = rosenbrock(X)
-
-f = 100*(X(2)-X(1)^2)^2+(1-X(1))^2; % rosenbrock function
-
-df = [400*X(1)^3+2*X(1)-400*X(1)*X(2)-2 ; -200*X(1)^2+200*X(2)]; % gradient of rosenbrock function
-
-hf = [1200*X(1)^2-400*X(2)+2 -400*X(1) ; -400*X(1) 200]; % hessian of rosenbrock function
-
-end
