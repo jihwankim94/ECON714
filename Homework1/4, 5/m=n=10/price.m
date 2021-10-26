fc44f97f5a01148bcd7265fbb40d227d7d6562b3
jihwankim94 <93222299+jihwankim94@users.noreply.github.com>
2021-10-26 15:37:04 -0400

    Add files via upload

---
+++ Homework1/4, 5/m=n=10/price.m
@@ -0,0 +1,35 @@
+function zero = price(p,x,w,sume)
+    
+global n m
+
+w_inverse = w.^(-1);
+
+for i = 1:m
+            
+        c = zeros(1,n);
+        d = zeros(n,1);
+        
+        for k = 1:n
+            
+            %c(k) = (p(i)/p(1))^(w_inverse(k,i));
+            %d(k) = x(k,i)^(w(k,i)*w_inverse(k,i));
+            
+            if i == 1
+                
+                c(k) = 1;
+                d(k) = x(k,i)^(w(k,i)*w_inverse(k,i));
+            
+            else
+                
+                c(k) = p(i-1)^(w_inverse(k,i));
+                d(k) = x(k,1)^(w(k,1)*w_inverse(k,i));
+                
+            end
+            
+        end
+        
+        zero(i) = sume(i)-c*d;
+    
+end
+
+end
